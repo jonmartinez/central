1f7f28d7da0a136285a137a4ac43d344709e8784
jonmartinez <jonmartinez@umariana.edu.co>
2015-02-25 07:26:28 -0500

    Aumento fucion para cambiar calc vieja

--- js/script.js
+++ js/script.js
@@ -15,3 +15,21 @@ function pulsada (tecla)
 		break;
 	}
 }
+
+function btn (tecla) 
+{
+	var listaNodosPantalla = document.getElementsByClassName('pantalla');
+	var nodoTextoPantalla = listaNodosPantalla[0].firstChild;
+	switch(tecla) {
+		case 'C':
+		nodoTextoPantalla.nodeValue = ' ';
+		break;
+		case '=':
+		var resultado = eval(nodoTextoPantalla.nodeValue);
+		nodoTextoPantalla.nodeValue = resultado;
+		break;
+		default:
+		nodoTextoPantalla.nodeValue = nodoTextoPantalla.nodeValue + tecla;
+		break;
+	}
+}
