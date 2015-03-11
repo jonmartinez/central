fa3c9d5926d88d37fd670eb036da799885f40a05
jonmartinez <jonmartinez@umariana.edu.co>
2015-03-11 09:05:36 -0500

    Escala de graficadora

--- js/script.js
+++ js/script.js
@@ -51,8 +51,13 @@ function btn (tecla)
 			var resultado = "Math.cos("+pantalla.value+")";
 			pantalla.value=resultado;
 		break;
-			case 'Graf':
-				document.getElementById('tmp').innerHTML='<canvas id="canvas" width="500" height="500" style="background-color:#FFFFF1;">Su navegador no permite utilizar canvas.</canvas><br>';
+		case 'Graf':
+	/*			document.getElementById('tmp').innerHTML='<canvas id="canvas" width="500" height="500" style="background-color:#FFFFF1;">Su navegador no permite utilizar canvas.</canvas><br>';
+				dibujarPlano(pantalla);*/
+				
+				ancho=document.getElementById('txtAncho').value;
+				alto=document.getElementById('txtAlto').value;
+				document.getElementById('tmp').innerHTML='<canvas id="canvas" width="'+ancho+'" height="'+alto+'" style="background-color:#FFFFF1;">Su navegador no permite utilizar canvas.</canvas><br>';
 				dibujarPlano(pantalla);
 			break;
 		default:
@@ -65,35 +70,67 @@ function dibujarPlano(pantalla)
 {
 	var canvas = document.getElementById('canvas');
 	var contexto = canvas.getContext('2d');
-	contexto.translate(250,250);
-	contexto.clearRect(-250, -250, canvas.width, canvas.height);
-	contexto.lineWidth=2; //Defino el ancho de la linea en pixeles
-	contexto.strokeStyle = '#00A8FF'; //Defino el color en hexagesimal
-//	contexto.restart();
- 
-	//EJEX
+	var ancho=canvas.width;
+	var alto=canvas.height;
+	contexto.translate(ancho/2,alto/2);
+	//contexto.clearRect(-250, -250, canvas.width, canvas.height);
+	contexto.lineWidth=1; //Defino el ancho de la linea en pixeles
+	contexto.strokeStyle = '#000'; //Defino el color en hexagesimal
+	
+	//	contexto.restart();
+ 	//EJEX
 	contexto.beginPath(); // Pongo el lapiz
-	contexto.moveTo(-250,0); // lo Ubico para iniciar el dibujo
-	contexto.lineTo(250,0); // trazo la linea hasta este punto
+	contexto.moveTo((ancho/2)*-1,0); // lo Ubico para iniciar el dibujo
+	contexto.lineTo((ancho/2),0); // trazo la linea hasta este punto
 	contexto.stroke(); // levanto el lapiz
 	contexto.closePath(); // me alisto para realizar otra parte del dibujo
 	//EJE Y
 	contexto.beginPath(); // Pongo el lapiz
-	contexto.moveTo(0,250);// lo Ubico para iniciar el dibujo
-	contexto.lineTo(0,-250);// trazo la linea hasta este punto
+	contexto.moveTo(0,alto);// lo Ubico para iniciar el dibujo
+	contexto.lineTo(0,alto*-1);// trazo la linea hasta este punto
 	contexto.stroke();// levanto el lapiz
 	contexto.closePath();// me alisto para realizar otra parte del dibujo	
+	//defino las escalas
+	var x = document.getElementById('txtEscalax').value;
+	var y = document.getElementById('txtEscalay').value;
+	var escalax=(ancho/2)/x;
+	var escalay=(alto/2)/y;
+	var i = (x)*(-1);
+	//escribo los puntos en x
+	puntosEnX(contexto,ancho/2,x,escalax);
+	//escribo los puntos en y
+	puntosEnY(contexto,alto/2,y,escalay);
 	
-	var escala=250/(document.getElementById('txtEscala').value);
-	var i = (document.getElementById('txtEscala').value)*(-1);
-	contexto.strokeStyle = '#000'; //Defino el color en hexagesimal
+	//escribir en otro color
+	contexto.strokeStyle = '#00A8FF'; //Defino el color en hexagesimal
 	contexto.beginPath();
 //	alert(pantalla.value);
 	for(x=i;x<=(i*-1);x+=0.2)
 	{
-		contexto.lineTo(x*escala,eval(pantalla.value)*(escala*-1));
-			contexto.stroke();
+		contexto.lineTo(x*escalax,eval(pantalla.value)*(escalay*-1));
+		contexto.stroke();
 	}
+}
 
-	contexto.translate(0,0);
+function puntosEnX(contexto,ancho,x,escalax)
+{
+	contador=x*-1;
+	for (i=ancho*-1;i<=ancho;i+=escalax) 
+	{
+		contexto.font = "bold 12px sans-serif";
+		contexto.fillText(contador,i,0);
+		contador++;
+	}
+}
+
+function puntosEnY(contexto,alto,y,escalay)
+{
+	contador=y;
+	for (i=alto*-1;i<=alto;i+=escalay) 
+	{
+		contexto.font = "bold 12px sans-serif";
+		contexto.fillText(contador,0,i);
+		contador--;
+		//contexto.fillText(i,0,(i*escalay-1)*-1);
+	}
 }
